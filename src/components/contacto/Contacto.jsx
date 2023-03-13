b0f4a602c2656bd5f1bfbdad0b1fd4334221b0bc
martinasorialanzi <martinasorialanzi@gmail.com>
2023-03-13 10:53:44 -0300

    emailjs en formulario

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React,{useRef} from "react";
 import "../contacto/contacto.css";
 import { Button, Container, Form } from "react-bootstrap";
+import emailjs from '@emailjs/browser'
 
 const Contacto = () => {
+  const form=useRef("")
+
+  const sendEmail=(e)=>{
+    e.preventDefault()
+
+    emailjs.sendForm('service_fsiawbl', 'template_8vcj1zs', form.current, 'KsX1c61JvBFU31_lx')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+  }
+
   return (
     <>
       <Container className="container">
@@ -12,26 +26,28 @@ const Contacto = () => {
         SALUDAR? PODES HACERLO ACÁ ABAJO 👇
         </div>
         <Container className="containerForm container ">
-          <Form className="formContacto">
+
+          <Form className="formContacto" ref={form} onSubmit={sendEmail}>
             <Form.Group className="mb-3" controlId="formContacto">
               <Form.Label>Nombre</Form.Label>
-              <Form.Control className="formControl" type="text"  maxLength={45}/>
+              <Form.Control className="formControl" type="text"  maxLength={45} name="user_name"/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formContacto">
+            <Form.Group className="mb-3" controlId="formContacto"  >
               <Form.Label>Email</Form.Label>
-              <Form.Control className="formControl" type="email"  />
+              <Form.Control className="formControl" type="email" name="user_email" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formContacto">
+            <Form.Group className="mb-3" controlId="formContacto" >
               <Form.Label>Mensaje</Form.Label>
               <Form.Control
                 type="text"
                 as="textarea"
                 rows={3}
                 className="formControl"
+                name="message"
               />
             </Form.Group>
 
-            <Button className="botonEnviar"  type="submit">
+            <Button className="botonEnviar"  type="submit" value="Send">
               Enviar
             </Button>
           </Form>
